e274a7fced1568ad1f793b9ec2ae42c47de0e224
Jayant Chowdhary <jchowdhary@google.com>
2019-02-15 10:31:54 -0800

    BasicCameraFactory: check if autofocus is available before setting it in requestTemplate.
    Bug: 119284303
    
    Test: N/A
    
    Change-Id: I5133d56d69f7a1f6e01d6682fff91202aaa9be17
    Signed-off-by: Jayant Chowdhary <jchowdhary@google.com>

--- src/com/android/camera/one/v2/common/BasicCameraFactory.java
+++ src/com/android/camera/one/v2/common/BasicCameraFactory.java
@@ -87,8 +87,15 @@ public class BasicCameraFactory {
             Observable<Boolean> hdrSceneSetting,
             int templateType) {
         RequestTemplate requestTemplate = new RequestTemplate(rootTemplate);
-        requestTemplate.setParam(
-              CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+        if (cameraCharacteristics.isAutoFocusSupported()) {
+            requestTemplate.setParam(
+                    CaptureRequest.CONTROL_AF_MODE,
+                    CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+        } else {
+            requestTemplate.setParam(
+                    CaptureRequest.CONTROL_AF_MODE,
+                    CaptureRequest.CONTROL_AF_MODE_OFF);
+        }
         requestTemplate.setParam(
               CaptureRequest.CONTROL_AE_MODE, new FlashBasedAEMode(flash, hdrSceneSetting));
         requestTemplate.setParam(

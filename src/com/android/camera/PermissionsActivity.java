d01659ef82cdf9af18e6e6639cbac8af91e9afde
Felix <google@ix5.org>
2019-05-30 03:19:13 +0200

    Rework location permissions request
    Don't ask for permissions if the user hasn't also opted into saving
    geolocation.
    
    Signed-off-by: Felix <google@ix5.org>
    Change-Id: Iab927e3202ea1b70e2cfd1b8671e3c900d2f2eb6

--- src/com/android/camera/PermissionsActivity.java
+++ src/com/android/camera/PermissionsActivity.java
@@ -121,10 +121,13 @@ public class PermissionsActivity extends QuickActivity {
             mFlagHasStoragePermission = true;
         }
 
-        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            mNumPermissionsToRequest++;
-            mShouldRequestLocationPermission = true;
+        if (mSettingsManager.getBoolean(SettingsManager.SCOPE_GLOBAL,
+            Keys.KEY_RECORD_LOCATION)) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                mNumPermissionsToRequest++;
+                mShouldRequestLocationPermission = true;
+            }
         }
 
         if (mNumPermissionsToRequest != 0) {
@@ -173,10 +176,6 @@ public class PermissionsActivity extends QuickActivity {
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
         Log.v(TAG, "onPermissionsResult counts: " + permissions.length + ":" + grantResults.length);
-        mSettingsManager.set(
-                SettingsManager.SCOPE_GLOBAL,
-                Keys.KEY_HAS_SEEN_PERMISSIONS_DIALOGS,
-                true);
 
         if (mShouldRequestCameraPermission) {
             if (grantResults.length > 0 && grantResults[mIndexPermissionRequestCamera] ==
@@ -204,6 +203,10 @@ public class PermissionsActivity extends QuickActivity {
         }
 
         if (mShouldRequestLocationPermission) {
+            mSettingsManager.set(
+                    SettingsManager.SCOPE_GLOBAL,
+                    Keys.KEY_HAS_SEEN_PERMISSIONS_DIALOGS,
+                    true);
             if (grantResults.length > 0 && grantResults[mIndexPermissionRequestLocation] ==
                     PackageManager.PERMISSION_GRANTED) {
                 // Do nothing

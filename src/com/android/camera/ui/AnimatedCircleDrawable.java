1a92306da109e3e35e02dd1a0433f347a2d72dbd
Anle Pan <anle.pan@nxp.com>
2023-04-17 16:15:09 +0000

    Fix monkey crash 'divide by zero' in Camera2
    When monkey test, there will be a probability that the values
    of in_min and in_max are equal(e.g.,both are 0),
    resulting in a division denominator of 0.
    Make a judgement before use to avoid crash
    
    Change-Id: I2231cb474cec16a583c14cd4737ef20425e192eb
    Signed-off-by: Anle Pan <anle.pan@nxp.com>

--- src/com/android/camera/ui/AnimatedCircleDrawable.java
+++ src/com/android/camera/ui/AnimatedCircleDrawable.java
@@ -22,6 +22,7 @@ import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
+import androidx.core.math.MathUtils;
 
 import com.android.camera.util.Gusterpolator;
 
@@ -135,6 +136,9 @@ public class AnimatedCircleDrawable extends Drawable {
      * @return The mapped value.
      */
     private static int map(int x, int in_min, int in_max, int out_min, int out_max) {
+        if (in_max == in_min) {
+            return MathUtils.clamp(x, out_min, out_max);
+        }
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
 
